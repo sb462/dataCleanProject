7a89b531cf0b0cca40ed8248a415d02c373c64f4
sb462 <sandip.biswas@gmail.com>
2014-08-19 21:06:43 -0500

    -08.19

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 unzip("./data/getdata-projectfiles-UCI HAR Dataset.zip", exdir ="./data")
-list.files("./data/UCI HAR Dataset/train")
+list.files("./data/UCI HAR Dataset/test")
 featureDataURL <- "./data/UCI HAR Dataset/train/X_train.txt"
 featureDataFrame <- read.table(featureDataURL,header=FALSE)
 featureCol <- read.table("./data/UCI HAR Dataset/features.txt",stringsAsFactors=FALSE)
@@ -7,3 +7,7 @@ library(data.table)
 setnames(featureCol, c("serialNo","featureName"))
 featureHeader <- featureCol$featureName
 setnames(featureDataFrame,featureHeader)
+featureTestDataURL <- "./data/UCI HAR Dataset/test/X_test.txt"
+featureTestDataFrame <- read.table(featureTestDataURL,header=FALSE)
+setnames(featureTestDataFrame,featureHeader)
+featureCompleteDataFrame<- rbind(featureDataFrame,featureTestDataFrame, deparse.level=0)
